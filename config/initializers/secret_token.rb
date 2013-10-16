b33ece787c88c41d602944da94cac5747aa62cad
Chuan Huang <chbenton@gmail.com>
2013-10-16 12:34:07 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+T::Application.config.secret_token = 'a5e2c9ed76699374ad1a874c16cf373739c7e1ecf78a363f350fcd9ddf99c52e565eebe233850c8adb812a2bcdd9b6b5f8e61e516c9b854dce62785d4ad92911'
---
+++ db/schema.rb
@@ -0,0 +1,39 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended to check this file into your version control system.
+
+ActiveRecord::Schema.define(:version => 20131008175409) do
+
+  create_table "posts", :force => true do |t|
+    t.string   "name"
+    t.string   "string"
+    t.text     "content"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "sysdiagrams", :primary_key => "diagram_id", :force => true do |t|
+    t.string  "name",         :limit => 128, :null => false
+    t.integer "principal_id",                :null => false
+    t.integer "version"
+    t.binary  "definition"
+  end
+
+  add_index "sysdiagrams", ["principal_id", "name"], :name => "UK_principal_name", :unique => true
+
+  create_table "tests", :force => true do |t|
+    t.string   "test"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+end
